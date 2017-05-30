eb00df9f2bc76ac9e0ca124a9db0a69015cd3b5e
Michael Stuart <mike.w.stuart@gmail.com>
2017-05-30 16:16:01 -0700

    event routes good to go

--- public/scripts/models/events.js
+++ public/scripts/models/events.js
@@ -1,27 +1,21 @@
 'use strict';
 
 navigator.geolocation.getCurrentPosition(position => {
-  const meetup = [];
-  const eventbrite =[];
   const latLon = [position.coords.latitude, position.coords.longitude]
-  console.log('pre-ajax1')
   $.ajax({
     url: '/venues/meetup',
     method: 'post',
     data: { latLon }
   })
   .then(data => {
-    console.log('meetup data', data)
-    meetup = data
-  })
-  .then(
+    console.log('meetup.com data:', data);
     $.ajax({
-    url: '/venues/eventbrite',
-    method: 'post',
-    data: { latLon }
-  }))
-  .then(data => {
-    console.log('data', data)
-    eventbrite = data
-  })
-})
+      url: '/venues/eventbrite',
+      method: 'post',
+      data: { latLon }
+    })
+    .then(data => {
+      console.log('eventbrite.com data:', data)
+    });
+  });
+});
