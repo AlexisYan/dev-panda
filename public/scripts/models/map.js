5d74ef2bf066c03983cccd42554f254bd06f9d28
RyleeAndrews <ryleeandrews73@gmail.com>
2017-05-31 16:05:16 -0700

    markers now populating!!

--- public/scripts/models/map.js
+++ public/scripts/models/map.js
@@ -1,15 +1,1 @@
-'use strict';
-let map;
-  function initMap(latLon = {lat: 47.618347, lng: -122.351977}) {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center:latLon ,
-      zoom: 12
-    });
-    data.forEach(venue => {
-      new google.maps.Marker({
-        position:{lat: venue.group.latLon[0], lon: venue.group.latLon[1] },
-        map: map,
-        title: 'Marker'
-      })
-    })
-  }
+
