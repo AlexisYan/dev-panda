a506af820c0ff15980b7a23de8e9d69abf4b4189
Michael Stuart <mike.w.stuart@gmail.com>
2017-06-01 00:08:25 -0700

    prep for heroku

--- public/scripts/views/aboutView.js
+++ public/scripts/views/aboutView.js
@@ -1,20 +1,24 @@
-function setTeasers() {
-  console.log($('#list-events p'));
-  $('#list-events p').hide();
-  $('.description').on('click', function() {
-    console.log('clicked description');
-    $('.description').text('Show Less -');
-    $('#list-events p').show();
-    $('#description').addClass('show-less');
-    $('#description').removeClass('description');
-  });
-};
+// function setTeasers() {
+//   console.log($('#list-events p'));
+//   $('#list-events p').hide();
+//   $('.description').on('click', function() {
+//     console.log('clicked description');
+//     $('.description').text('Show Less -');
+//     $('#list-events p').show();
+//     $('#description').addClass('show-less');
+//     $('#description').removeClass('description');
+//   });
+// };
 
-function hideTag() {
-  $('.show-less').on('click', function(){
-    console.log('cant hide p tags');
-    $('#list-events p').hide();
-    $('#description').addClass('description');
-    $('#description').removeClass('show-less');
-  })
+const setTeasers = () => {
+  $('#list-events p').hide();
 }
+
+// function hideTag() {
+//   $('.show-less').on('click', function(){
+//     console.log('cant hide p tags');
+//     $('#list-events p').hide();
+//     $('#description').addClass('description');
+//     $('#description').removeClass('show-less');
+//   })
+// }
