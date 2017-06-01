6fbf34ecb6fd873f9c7e16d00b3bd1ac077685cf
Stephanie Dover <doverstephaniem@gmail.com>
2017-06-01 14:01:08 -0700

    fixed hide and show description funcitons

--- public/scripts/views/eventView.js
+++ public/scripts/views/eventView.js
@@ -4,11 +4,22 @@ function setTeasers() {
   console.log($('#list-events p'));
   $('#list-events p').hide();
   $('.description').on('click', function(e) {
-    console.log(this.childNodes);
-    $(this.childNodes).show()
+    console.log('show',this.parentNode.childNodes);
+    $(this.parentNode.childNodes).fadeIn(150)
     $(this).text('Show Less -');
-    // $('#description').addClass('show-less');
-    // $('#description').removeClass('description');
+    $(this).addClass('show-less');
+    $(this).removeClass('description');
+    hideDescription();
+  });
+};
+function hideDescription() {
+  $('.show-less').off().on('click', function(e) {
+    console.log(e.target, 'hide', this.siblings);
+    $(this).siblings().hide()
+    $(this).text('Description ->');
+    $(this).addClass('description');
+    $(this).removeClass('show-less');
+    setTeasers();
   });
 };
 
