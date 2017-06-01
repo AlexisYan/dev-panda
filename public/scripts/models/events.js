e53ea290da1a9dee2e39080bc8b6ced9dfd26c3b
Michael Stuart <mike.w.stuart@gmail.com>
2017-06-01 16:53:55 -0700

    add panda icons to map

--- public/scripts/models/events.js
+++ public/scripts/models/events.js
@@ -30,7 +30,7 @@ const appendMapMarker = event => {
   let marker = new google.maps.Marker({
     position:{lat: event.group.lat, lng: event.group.lon},
     map,
-    title: 'Marker',
+    title: icon: { url: 'icons/panda.svg' }
   });
   let infoWindow = new google.maps.InfoWindow({
     content: `${event.description}`
